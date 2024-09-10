9bd46cf0caaa6d85f21932b30f6a1874e0e31f91
shreyash-01 <singhshreyash0075@gmail.com>
2024-09-10 09:22:36 +0530

    Initialised project

---
+++ src/main/java/com/example/springapiliveasy/SpringApiLiveasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springapiliveasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiLiveasyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiLiveasyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springapiliveasy/SpringApiLiveasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapiliveasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiLiveasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
